e832a04befe475dd5e67c10d3d69e77fa70dbe43
杨 珂 <yangke@hualala.com>
2017-03-27 23:13:11 +0800

    170327

--- helpers/polyfill.js
+++
@@ -1,5 +0,0 @@
-import './prototype'
-
-if (process.env.__CLIENT__ !== true) {
-  global.URLSearchParams = require('./URLSearchParams')
-}
--- helpers/prototype.js
+++
@@ -1,5 +0,0 @@
-String.prototype.format = function (...args) {
-  return this.replace(/\{(\d+)\}/g, function (m, i) {
-    return args[i]
-  })
-}
